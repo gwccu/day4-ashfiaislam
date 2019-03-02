c23a8873e8031c4680948b2b0336cc92289bdee3
ashfiaislam <ashfia.zara@gmail.com>
2019-03-02 17:26:00 +0000

    ch

--- warmupDay4.py
+++ warmupDay4.py
@@ -1,10 +1,8 @@
 # File name: warmupDay4.py
-from random import randint
-player = input( ' rock (r) paper (p) or scissors (s)?')
-print(player, 'vs')
-player = input( ' rock (r), paper (p) or scissors (s)? ')
-print(player, 'vs')
-chosen = randint(1,3)
-print(chosen)
-chosen = randint(1,3)
-print(chosen) == 1:
+strength = 100
+while strength < 200 :
+    if(strength % 2 == 0):
+        print(strength, "is even")
+    else:
+         print(strength, "is odd")
+    strength +=1
